a63e567ef0e5a640d6b0a86edf20fd76a6aad5df
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2016-01-04 22:28:08 +0100

    Empty model introduced

---
+++ Paint/Model/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Paint.Model
+{
+  public class ToolModel
+  {
+    internal void UpdateTool(Tool chosenTool)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
